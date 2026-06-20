676cda0c788e0b340cbb6c6272c6874b5c6efee9
OmniFire <nurisbullah81@gmail.com>
2026-06-21 00:44:07 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,41 @@
-loadstring(game:HttpGet("https://www.luxyhub.space/api/loader/luxyhub"))()
+local a = getgenv and getgenv()
+if not a then
+	return
+end
+
+if a.luxy_router_debounce and (tick() - a.luxy_router_debounce) <= 5 then
+	return
+end
+a.luxy_router_debounce = tick()
+
+if not game:IsLoaded() then
+	game.Loaded:Wait()
+end
+
+local b = {
+	[89469502395769] = "Kalb.lua",
+	[107646426076756] = "build-a-ring-farm.lua",
+	[92416421522960] = "slime-rng.lua",
+	[95082159892680] = "speed-keyboard-escape.lua",
+	[118941584817777] = "speed-keyboard-escape.lua",
+}
+
+local c = b[game.PlaceId]
+if not c then
+	return
+end
+
+pcall(function()
+	if not loadstring then
+		return
+	end
+
+	local d = tostring(math.random(10000, 99999))
+
+	local e = "https://raw.githubusercontent.com/Omnie7/Luxy-Hub/refs/heads/main/Game/" .. c .. "?nocache=" .. d
+
+	local f = game:HttpGet(e)
+	if f and f ~= "" then
+		loadstring(f)()
+	end
+end)
