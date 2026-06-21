b507f196e2c50459efae2b23cdbfbee4173f970f
OmniFire <nurisbullah81@gmail.com>
2026-06-22 03:44:43 +0700

    Update main.lua

--- main.lua
+++ main.lua
@@ -13,28 +13,28 @@ if not game:IsLoaded() then
 end
 
 local b = {
-	[89469502395769] = "Kalb.lua",
+	[89469502395769] = "kalb.lua",
 	[95082159892680] = "speed keyboard escape.lua",
 	[118941584817777] = "speed keyboard escape.lua",
 	[97598239454123] = "grow a garden 2.lua",
 }
-
 local c = b[game.PlaceId]
 if not c then
 	return
 end
 
 pcall(function()
-	if not loadstring then
-		return
-	end
+    if not loadstring then
+        return
+    end
 
-	local d = tostring(math.random(10000, 99999))
+    local d = tostring(math.random(10000, 99999))
 
-	local e = "https://raw.githubusercontent.com/Omnie7/Luxy-Hub/refs/heads/main/Game/" .. c .. "?nocache=" .. d
+    local e = "https://raw.githubusercontent.com/Omnie7/Luxy-Hub/refs/heads/main/Game/" .. c .. "?nocache=" .. d
 
-	local f = game:HttpGet(e)
-	if f and f ~= "" then
-		loadstring(f)()
-	end
+    local f = game:HttpGet(e)
+    if f and f ~= "" then
+        getgenv().LUXY_SECURE_LOAD = true 
+        loadstring(f)()
+    end
 end)
